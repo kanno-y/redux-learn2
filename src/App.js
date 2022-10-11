98f0bd82680d104f51ecd28125df581031075c96
kanno-y <kanno0521@gmail.com>
2022-10-11 14:37:56 +0900

    connectを利用してstateを取得する

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import store from "./store/index";
 import "./App.css";
+import { connect } from "react-redux";
+import Count from "./components/Count";
 
-function App() {
-  console.log(store);
+function App({ count }) {
   return (
     <div className="App">
       <h1>Redux learn</h1>
       <p>Count: {store.getState().count}</p>
+      <Count />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { count: state.count };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
